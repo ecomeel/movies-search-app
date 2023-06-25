bf99d79f216370280c93a5b82f6faf3a8eb9c907
ecomeel <ecomeel@gmail.com>
2023-06-25 13:17:30 +0300

    render movies

--- js/index.js
+++ js/index.js
@@ -7,38 +7,60 @@
 //     .then(response => response.json())
 //     .then(res => console.log(res))
 
-const searchMovieInputNode = document.getElementById('searchMovieInput');
-const searchMovieBtnNode = document.getElementById('searchMovieBtn');
-const searchResultNode = document.getElementById('searchResult');
+const searchMovieInputNode = document.getElementById("searchMovieInput");
+const searchMovieBtnNode = document.getElementById("searchMovieBtn");
+const searchResultNode = document.getElementById("searchResult");
 
 const isSearchEmpty = (result) => {
-    return result ?  false :  true
-}
+    return result ? false : true;
+};
 
 const getSearchEntry = () => {
     return searchMovieInputNode.value;
-}
+};
+
+const renderMovies = (movies) => {
+    let moviesHTML = "";
+    movies.forEach((movie) => {
+        const posterLink = movie.Poster;
+        const title = movie.Title;
+        const year = movie.Year;
+        const type = movie.Type;
+
+        moviesHTML =
+            moviesHTML +
+            `
+                        <div>
+                            <img src=${posterLink}>
+                            <h2>${title}</h2>
+                            <p>${year}</p>
+                            <p>${type}</p>
+                        </div>
+                    `;
+        searchResultNode.innerHTML = moviesHTML;
+    });
+};
 
 const findMovies = (searchEntry) => {
-    const API = `https://www.omdbapi.com/?apikey=f0bbff55&s=${searchEntry}`
+    const API = `https://www.omdbapi.com/?apikey=f0bbff55&s=${searchEntry}`;
     fetch(API)
-        .then(response => response.json())
+        .then((response) => response.json())
         .then((res) => {
-            if (res.Response == 'False') {
-                searchResultNode.innerText = 'Movies not found';
+            if ((res.Response == "False") & (res.Error == "Movie not found!")) {
+                searchResultNode.innerText = "Movies not found";
                 // Добавить стили
             } else {
                 const movies = res.Search;
-                console.log(movies)
+                // console.log(movies);
+                renderMovies(movies)
             }
-        })
-}
+        });
+};
 
 const searchMovieHandler = () => {
     const searchEntry = getSearchEntry();
-    if (isSearchEmpty(searchEntry)) return
+    if (isSearchEmpty(searchEntry)) return;
     findMovies(searchEntry);
+};
 
-}
-
-searchMovieBtnNode.addEventListener('click', searchMovieHandler);
+searchMovieBtnNode.addEventListener("click", searchMovieHandler);
