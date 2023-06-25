01dd347152434b7557abd515d8f2f4e5ee0ab45b
ecomeel <ecomeel@gmail.com>
2023-06-25 12:54:09 +0300

    start styles & html & not found js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
-console.log('first')
+// const params = new URLSearchParams(location.search);
+// console.log(params)
+// const id = params.get('id')
+// console.log(id)
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then(response => response.json())
+//     .then(res => console.log(res))
+
+const searchMovieInputNode = document.getElementById('searchMovieInput');
+const searchMovieBtnNode = document.getElementById('searchMovieBtn');
+const searchResultNode = document.getElementById('searchResult');
+
+const isSearchEmpty = (result) => {
+    return result ?  false :  true
+}
+
+const getSearchEntry = () => {
+    return searchMovieInputNode.value;
+}
+
+const findMovies = (searchEntry) => {
+    const API = `https://www.omdbapi.com/?apikey=f0bbff55&s=${searchEntry}`
+    fetch(API)
+        .then(response => response.json())
+        .then((res) => {
+            if (res.Response == 'False') {
+                searchResultNode.innerText = 'Movies not found';
+                // Добавить стили
+            } else {
+                const movies = res.Search;
+                console.log(movies)
+            }
+        })
+}
+
+const searchMovieHandler = () => {
+    const searchEntry = getSearchEntry();
+    if (isSearchEmpty(searchEntry)) return
+    findMovies(searchEntry);
+
+}
+
+searchMovieBtnNode.addEventListener('click', searchMovieHandler);
